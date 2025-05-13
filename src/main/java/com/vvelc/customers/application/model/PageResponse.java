068a35f739e88fc29f1148a560d316da89c266ff
Victor Velazquez Cid <76626605+vvelc@users.noreply.github.com>
2025-05-13 12:38:36 -0400

    feat: agregar modelo de respuesta paginada

--- src/main/java/com/vvelc/customers/application/model/PageResponse.java
+++ src/main/java/com/vvelc/customers/application/model/PageResponse.java
@@ -1,10 +1,16 @@
 package com.vvelc.customers.application.model;
 
+import lombok.*;
+
 import java.util.List;
 
-public record PageResponse<T>(
-        List<T> items,
-        int page,
-        int size,
-        Long total
-) {}
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+public class PageResponse<T> {
+    private List<T> items;
+    private int page;
+    private int size;
+    private Long total;
+}
